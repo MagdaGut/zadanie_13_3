3ef1c9ef9f8f24dfd66108393915ebcd3fb8279f
Magda Gutowska <m.gutowska0909@gmail.com>
2018-06-08 00:12:35 +0200

    Fix program.js

--- program.js
+++ program.js
@@ -17,19 +17,27 @@ process.stdin.setEncoding('utf-8');
 //console.log('node --version');
 
 
+process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function() {
-   
     var input = process.stdin.read();
-    switch (input !== null) {
+
+    if (input !== null) {
         var instruction = input.toString().trim();
-        case (instruction === '/exit') 
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-            break;
-         default: 
-            process.stderr.write('Wrong instruction!');
         
+        switch (instruction) {        
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/lang':
+                process.stdout.write(process.env.LANG);
+                break;
+            case '/version':
+                process.stdout.write(process.versions.node);
+                break;
+            default: 
+                process.stderr.write('Wrong instruction!');     
+        }
     }
 });
-
-console.log(process.env);
