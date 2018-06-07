0eed7375070c334bcad6a8427500b9bf0d734df1
Magda Gutowska <m.gutowska0909@gmail.com>
2018-06-07 20:13:02 +0200

    Add files

---
+++ program.js
@@ -0,0 +1,35 @@
+process.stdin.setEncoding('utf-8');
+
+/*process.stdin.on('readable', function() {
+    
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === 'exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});*/
+
+//console.log('node --version');
+
+
+process.stdin.on('readable', function() {
+   
+    var input = process.stdin.read();
+    switch (input !== null) {
+        var instruction = input.toString().trim();
+        case (instruction === '/exit') 
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+         default: 
+            process.stderr.write('Wrong instruction!');
+        
+    }
+});
+
+console.log(process.env);
